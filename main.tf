c97bbffda020268952d5331d0a67da97e88792fd
mshoaibshafi@yahoo.com <mshoaibshafi@yahoo.com>
2023-03-31 09:47:48 -0700

    added files

---
+++ main.tf
@@ -0,0 +1,49 @@
+# Local variables
+locals { users = yamldecode(file("users.yaml")) }
+locals { groups = yamldecode(file("groups.yaml")) }
+
+# Create iam user
+resource "aws_iam_user" "users" {
+  for_each = local.users
+  name     = each.value.name
+}
+
+# Create iam group
+resource "aws_iam_group" "groups" {
+  for_each = local.groups
+  name     = each.value.name
+}
+
+# Users can have multiple groups 
+# Flatten user > group for group-assignments
+locals {
+  groupAssignments = flatten([
+    for key, attributes in local.users : [
+      for g in attributes.groups : {
+        name  = attributes.name
+        group = g
+        email = key
+      }
+    ]
+  ])
+}
+
+# Assign Users to Group
+resource "aws_iam_user_group_membership" "groups" {
+  for_each = { for idx, v in local.groupAssignments : idx => v }
+  user     = aws_iam_user.users[each.value.email].name
+  groups   = [aws_iam_group.groups[each.value.group].name]
+}
+
+
+## Provider 
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.59.0"
+    }
+  }
+}
+
+provider "aws" {}
